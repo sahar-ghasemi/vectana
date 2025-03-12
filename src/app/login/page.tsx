75ea502ae3c803c8b984d4fe206b73f5a26ec518
sahar <saharghasemi7711@gmail.com>
2025-03-13 10:03:44 +1100

    chore: design initial layout for login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+  return (
+    <>
+      <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            ایمیل:
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5"
+            placeholder="name@flowbite.com"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            پسورد{" "}
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              value=""
+              className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              required
+            />
+          </div>
+          <label
+            htmlFor="remember"
+            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            بخاطر سپردن من{" "}
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-purple-700 hover:bg-purple-800 cursor-pointer font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          ورود
+        </button>
+      </form>
+
+      <Link href="/signup">ثبت نام</Link>
+    </>
+  );
+}
--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function LoginButton() {
   return (
-    <button
-      type="button"
+    <Link
       className="text-white bg-purple-600 hover:bg-purple-700 cursor-pointer font-medium rounded-lg text-sm px-4 py-2 text-center"
+      href="/login"
     >
       ورود به سایت
-    </button>
+    </Link>
   );
 }
