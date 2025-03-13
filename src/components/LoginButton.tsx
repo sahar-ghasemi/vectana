080cc80c9c1bb54377f066e9b07a4199e8e4d13c
sahar <saharghasemi7711@gmail.com>
2025-03-13 12:27:01 +1100

    Add custom color scheme to Tailwind CSS configuration in global css file

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function LoginButton() {
   return (
     <Link
-      className="text-white bg-purple-600 hover:bg-purple-700 cursor-pointer font-medium rounded-lg text-sm px-4 py-2 text-center"
+      className="text-white bg-themeDefaultPurple hover:bg-themeDarkPurple cursor-pointer font-medium rounded-lg text-md px-6 py-2 text-center"
       href="/login"
     >
       ورود به سایت
