88bce5f40dabf90049df4ae5368d1e3b4fa5b241
sahar <saharghasemi7711@gmail.com>
2025-05-11 01:48:30 +1000

    fix: prevent login/signup button flicker by handling session loading state in PublicNav
    - Hide auth buttons while session is loading
    - Show dashboard button only for authenticated users
    - Show login and signup button fot unauthenticated users

--- src/components/PublicNav.tsx
+++ src/components/PublicNav.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { useState } from "react";
 import { LoginButton, SignupButton } from "./";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { User } from "lucide-react";
 
 export default function PublicNav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,6 +15,7 @@ export default function PublicNav() {
     { label: "تماس با ما", href: "/contact" },
     { label: "درباره ما", href: "/about" },
   ];
+  const { data: session, status } = useSession();
   return (
     <>
       <nav className="bg-white border-gray-200 drop-shadow-xs">
@@ -31,8 +34,24 @@ export default function PublicNav() {
           </a>
           {/* ورود به سایت   */}
           <div className="flex items-center space-x-6 rtl:space-x-reverse">
-            <SignupButton />
-            <LoginButton />
+            {status === "loading" ? (
+              <></>
+            ) : status === "authenticated" ? (
+              <Link
+                href="/dashboard"
+                className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white cursor-pointer"
+              >
+                <span className="relative px-5 py-2 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-transparent flex items-center gap-2">
+                  <User className="w-5 h-5" />
+                  ورود به داشبورد
+                </span>
+              </Link>
+            ) : (
+              <>
+                <SignupButton />
+                <LoginButton />
+              </>
+            )}
           </div>
         </div>
         <hr className=" h-0.5 border-t-0 bg-neutral-100" />
