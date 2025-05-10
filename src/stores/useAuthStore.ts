02cf3d4bceaa105873eb9eb3635410388c33f404
sahar <saharghasemi7711@gmail.com>
2025-05-10 12:43:21 +1000

    feat(store-zustand): add isAuthChecked state to auth store

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -8,16 +8,20 @@ export type User = {
 
 interface AuthState {
   isLoggedIn: boolean;
+  isAuthChecked: boolean;
   user: User | null;
   setLoggedIn: (val: boolean) => void;
   setUser: (user: User | null) => void;
+  setAuthChecked: (val: boolean) => void;
   logout: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   isLoggedIn: false,
+  isAuthChecked: false,
   user: null,
   setLoggedIn: (val) => set({ isLoggedIn: val }),
   setUser: (user) => set({ user }),
+  setAuthChecked: (val) => set({ isAuthChecked: val }),
   logout: () => set({ isLoggedIn: false, user: null }),
 }));
